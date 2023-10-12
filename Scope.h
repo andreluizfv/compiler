4b2d1c0cbc2b3193ad406fd0fb1c7420889b2724
andreluizfv <andreluiz.lf11@hotmail.com>
2023-10-12 20:29:00 -0300

    Scope libraries added

---
+++ Scope.h
@@ -0,0 +1,23 @@
+#ifndef COMPILER_TOKEN_H
+#define COMPILER_TOKEN_H
+#include <vector>
+using namespace std;
+namespace Scope
+{
+    class scope_object{
+    public:
+        int nName;
+        scope_object(int nName);
+    };
+
+    vector<vector<scope_object>> SymbolTable;
+    int nCurrentLevel = -1;
+
+    int NewBlock();
+    int EndBlock();
+    scope_object Define(int tokenSecond);
+    scope_object SearchWhenDeclared(int tokenSecond);
+    scope_object FindWhenUsed(int tokenSecond);
+}
+
+#endif // COMPILER_TOKEN_H
