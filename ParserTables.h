084633a5279ee2d3a8da101a063b63cd1fab8f5c
Pedro <pbbdasilva@gmail.com>
2023-10-14 14:06:59 -0300

    remove tokentype from wrong header file

--- ParserTables.h
+++ ParserTables.h
@@ -40,63 +40,6 @@ typedef enum {
   _Y,
 } non_term;
 
-enum TokenType {
-  // palavras reservadas
-  ARRAY,
-  BOOLEAN,
-  BREAK,
-  CHAR,
-  CONTINUE,
-  DO,
-  ELSE,
-  FALSE,
-  FUNCTION,
-  IF,
-  INTEGER,
-  OF,
-  RETURN,
-  STRING,
-  STRUCT,
-  TRUE,
-  TYPE,
-  VAR,
-  WHILE,
-  // simbolos
-  COLON,
-  SEMI_COLON,
-  COMMA,
-  EQUALS,
-  LEFT_SQUARE,
-  RIGHT_SQUARE,
-  LEFT_BRACES,
-  RIGHT_BRACES,
-  LEFT_PARENTHESIS,
-  RIGHT_PARENTHESIS,
-  AND,
-  OR,
-  LESS_THAN,
-  GREATER_THAN,
-  LESS_OR_EQUAL,
-  GREATER_OR_EQUAL,
-  NOT_EQUAL,
-  EQUAL_EQUAL,
-  PLUS,
-  PLUS_PLUS,
-  MINUS,
-  MINUS_MINUS,
-  TIMES,
-  DIVIDE,
-  DOT,
-  NOT,
-  // tokens regulares
-  CHARACTER,
-  NUMERAL,
-  STRINGVAL,
-  ID,
-  // token deconhecido
-  UNKNOWN
-};
-
 static int action_table[172][85] = {
     {
         0, 0, 0, 3, 4, 5, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,
